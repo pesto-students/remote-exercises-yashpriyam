98f86c0ccdd0d484e4c27893bc7d1d8c47ec97df
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-06-06 10:19:24 +0530

    removed unnecessary sorting before counting the frequency of elements

--- js-exercises/freq-sort/freqSort.js
+++ js-exercises/freq-sort/freqSort.js
@@ -1,7 +1,6 @@
 function frequencySort(inputArray) {
   const frequencyObject = {};
   inputArray
-    .sort()
     .map(el => frequencyObject[el] ? frequencyObject[el] += 1 : frequencyObject[el] = 1);
   const keysArray = Object.keys(frequencyObject);
   keysArray.sort((curr, next) => frequencyObject[next] - frequencyObject[curr]);
