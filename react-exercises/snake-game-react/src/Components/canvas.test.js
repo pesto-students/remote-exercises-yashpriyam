fb1d7da1f8f2ec1f5018f8fde648982e6031fcfa
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-06-21 23:35:09 +0530

    removed dummy test from canvas.test.js

--- react-exercises/snake-game-react/src/Components/canvas.test.js
+++ react-exercises/snake-game-react/src/Components/canvas.test.js
@@ -4,12 +4,3 @@ import Adapter from 'enzyme-adapter-react-16';
 import Canvas from './canvas';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-describe('snake', () => {
-  it('snake length', () => {
-    let snakeArray = [...Array(5).keys()].map(x => [0, 2*x]);
-    const wrapper = render(<Canvas snakeSquares={snakeArray}/>);
-    const snakelength = wrapper['0'].children.length;
-    expect(snakelength).toBe(5);
-  })
-})
